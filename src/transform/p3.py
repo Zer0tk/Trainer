bcc42dccefbb2e617f8bd728f475970cc2b5961f
Товарищ Артём <ahlushanenko@gmail.com>
2026-09-07 07:21:02 +0300

    Fix: rewritten more precisely

--- src/transform/p3.py
+++ src/transform/p3.py
@@ -9,7 +9,7 @@ db_config = {
 
 host = db_config["connection"]["host"]
 port = db_config["connection"]["port"]
-ssl_key = db_config["connection"].get("ssl_settings") if db_config["connection"].get("ssl_settings") else "verify-full"
+ssl_key = db_config["connection"].get("ssl_settings", {}).get("ssl_mode", "verify-full")
 
 db_config["connection"]["user"] = "admin"
 db_config["connection"]["max_connections"] = 100
